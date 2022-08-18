8a444501242517a23168f282d5486de8a8bd1fb4
joker-jonesy <spashmcgrath94@yahoo.com>
2022-08-18 17:02:12 -0400

    Kermit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,17 @@
 import React from "react";
 function Contact(props){
 
-    const [contacted, setContacted]=React.useState(false)
+    const [contacted, setContacted]=React.useState(false);
+
+    const returnClass = ()=>{
+        return contacted ? "contacted":"not-contacted"
+    }
 
     return(
         <div>
             <h1>{props.data.name}</h1>
             <p>{props.data.adr}</p>
-            <div className={contacted ? "contacted":"not-contacted"}>
+            <div className={"contact "+returnClass()}>
                 {contacted ? "Has Been Contacted!": "Needs to be contacted!"}
             </div>
         </div>
